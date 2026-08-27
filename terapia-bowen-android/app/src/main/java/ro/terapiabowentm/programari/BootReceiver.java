4456b01c9fef2752112f783bfb180f64e4b39532
AutoID <121750896+AutoID-Alex@users.noreply.github.com>
2026-08-27 20:43:54 +0300

    Add boot receiver

---
+++ terapia-bowen-android/app/src/main/java/ro/terapiabowentm/programari/BootReceiver.java
@@ -0,0 +1,7 @@
+package ro.terapiabowentm.programari;
+
+import android.content.*;
+
+public class BootReceiver extends BroadcastReceiver {
+    @Override public void onReceive(Context context, Intent intent){ LeadCheckReceiver.schedule(context); }
+}
